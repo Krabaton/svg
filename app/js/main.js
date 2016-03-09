9410dfa6dcc4e0f65cded2fd3d316bc611fd5f78
pangurban <pangurban.nn@gmail.com>
2016-03-09 11:55:39 +0400

    So much pretty.

--- app/js/main.js
+++ app/js/main.js
@@ -1,97 +1,70 @@
-$(document).ready(function() {
+(function() {
+  'use strict';
+
   var date = new Date(),
-    day = date.getDate(),
-    month = date.getMonth(),
-    year = date.getFullYear(),
-    valcode = [],
-    exchange = [];
-
-  day = day;
-  day = (day < 10) ? '0' + day : day;
-  month = (month < 9) ? '0' + (++month) : (++month);
-
-  var getEUR = function() {
-    var d = $.Deferred();
-    $.ajax({
-      url: 'http://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=EUR&date=' + year + month + day,
-      type: 'GET',
-      success: function(res) {
-        var headline = $(res.responseText).find('cc').text(),
-          rate = $(res.responseText).find('rate').text();
-        valcode.push(headline);
-        exchange[headline] = rate;
-        d.resolve();
-      }
-    });
-    return d;
-  };
-
-  var getUSD = function() {
-    var d = $.Deferred();
-    $.ajax({
-      url: 'http://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=USD&date=' + year + month + day,
-      type: 'GET',
-      success: function(res) {
-        var headline = $(res.responseText).find('cc').text(),
-          rate = $(res.responseText).find('rate').text();
-        valcode.push(headline);
-        exchange[headline] = rate;
-        d.resolve();
-      }
-    });
-    return d;
-  };
-
-  var getGBP = function() {
-    var d = $.Deferred();
-    $.ajax({
-      url: 'http://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=GBP&date=' + year + month + day,
-      type: 'GET',
-      success: function(res) {
-        var headline = $(res.responseText).find('cc').text(),
-          rate = $(res.responseText).find('rate').text();
-        valcode.push(headline);
-        exchange[headline] = rate;
-        d.resolve();
-      }
-    });
-    return d;
-  };
-
-
-  $.when(
-      getEUR(),
-      getUSD(),
-      getGBP())
-    .done(function() {
-      $('.result').highcharts({
-        chart: {
-          type: 'column',
-          renderTo: 'charts',
-          defaultSeriesType: 'bar'
-        },
-        title: {
-          text: 'Курсы основных валют'
-        },
-        xAxis: {
-          categories: ['EUR', 'USD', 'GBP']
-        },
-        yAxis: {
-          title: {
-            text: ''
-          }
-        },
-        plotOptions: {
-            column: {
-                pointWidth: 100,
-            }
-        },
-        series: [{
-          showInLegend: false,
-          name: 'текущий курс',
-          data: [+exchange['EUR'], +exchange['USD'], +exchange['GBP']]
-        }]
+      valcode = [],
+      exchange = [],
+      stringDate = date.toLocaleDateString('ru', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
       });
 
+  stringDate = stringDate.split('.').reverse().join('');
+
+  var apiUrls = ['http://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=GBP&date=' + stringDate,
+                 'http://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=EUR&date=' + stringDate,
+                 'http://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=USD&date=' + stringDate];
+
+  function httpGet(apiUrl) {
+    return new Promise(function(resolve, reject) {
+      $.ajax({
+        url: apiUrl,
+        type: 'GET',
+        success: function(response) {
+          var headline = $(response.responseText).find('cc').text(),
+              rate = $(response.responseText).find('rate').text();
+
+          valcode.push(headline);
+          exchange[headline] = rate;
+          resolve();
+        }
+      });
     });
-});
+  }
+
+  function createHighcharts() {
+    $('.result').highcharts({
+      chart: {
+        type: 'column',
+        renderTo: 'charts',
+        defaultSeriesType: 'bar'
+      },
+      title: {
+        text: 'Курсы основных валют'
+      },
+      xAxis: {
+        categories: ['EUR', 'USD', 'GBP']
+      },
+      yAxis: {
+        title: {
+          text: ''
+        }
+      },
+      plotOptions: {
+          column: {
+              pointWidth: 100,
+          }
+      },
+      series: [{
+        showInLegend: false,
+        name: 'текущий курс',
+        data: [+exchange['EUR'], +exchange['USD'], +exchange['GBP']]
+      }]
+    });
+  }
+
+  Promise
+    .all(apiUrls.map(httpGet))
+    .then(createHighcharts);
+})();
